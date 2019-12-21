459811ddac6caf4180e3a6f591d0e6792e2b3d96
tdiallo <thiernoamiroud@gmail.com>
2019-12-21 13:03:50 -0500

    added sample test for jenkins ok

--- src/test/java/com/tamirou/springboot/jwt/JWTSpringSecurity/security/services/UserDetailsServiceImplTest.java
+++ src/test/java/com/tamirou/springboot/jwt/JWTSpringSecurity/security/services/UserDetailsServiceImplTest.java
@@ -1,5 +1,13 @@
+package com.tamirou.springboot.jwt.JWTSpringSecurity.security.services;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class UserDetailsServiceImplTest {
 
+    @Test
+    void loadUserByUsername() {
+        assertEquals(1,1);
+    }
 }
